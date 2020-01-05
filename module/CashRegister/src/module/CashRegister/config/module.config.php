19b314bcb0d15f44e47d1bc7b75d5696c60460d7
jolantaklima <34963842+jolantaklima@users.noreply.github.com>
2020-01-05 18:02:48 +0100

    Routing & controllers

---
+++ module/CashRegister/src/module/CashRegister/config/module.config.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CashRegister;
+
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'controllers' => [
+        'factories' => [
+            Controller\CashRegisterController::class => InvokableFactory::class,
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\CashRegisterController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            'album' => __DIR__ . '/../view',
+        ],
+    ],
+];
---
+++ module/CashRegister/src/module/CashRegister/src/Controller/CashRegisterController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CashRegister\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class CashRegisterController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
